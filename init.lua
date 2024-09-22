19be6ffa3a5fe876a92331f2f18e24ed27c3d63c
falco <>
2024-09-22 20:36:24 +0200

    adjust flash settings

--- init.lua
+++ init.lua
@@ -33,8 +33,7 @@ local plugins =  {
     opts = {},
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     },
   },
 };
